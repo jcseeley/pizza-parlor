b26f407cfcd23a05a90d8e7908eb4ed590bb9aa8
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-28 11:05:24 -0800

    implement code for Pizza.prototype.toppingsPrice().

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,21 @@ function Pizza(size, toppings, price) {
 }
 
 Pizza.prototype.sizePrice = function() {
-  if (this.size = 12) {
+  if (this.size === 14) {
+    this.price = 14;
+  } else if (this.size === 16) {
+    this.price = 16;
+  } else {
     this.price = 12;
-    return this.price;
-  }
+  } return this.price;
+}
+
+Pizza.prototype.toppingsPrice = function() {
+  if (this.toppings["length"] === 2) {
+    this.price += 2;
+  } else if (this.toppings["length"] > 2 && this.toppings <= 5) {
+    this.price += 8;
+  } else if (this.toppings["length"] > 5 && this.toppings <= 8) {
+    this.price += 12;
+  } return this.price;
 }
